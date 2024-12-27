cb900c9be0c6244517c4eef1dbffaf9a765c1c53
Thanhnv2923 <thanhnv2923@gmail.com>
2024-12-27 17:40:51 +0700

    code fix ok 17:40 27/12/2024

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Đảm bảo đường dẫn mặc định khi build
+  },
 })
