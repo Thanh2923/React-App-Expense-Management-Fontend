e057c7a8d318102226a538d85655b1b886ff27eb
Thanhnv2923 <thanhnv2923@gmail.com>
2024-12-28 10:18:21 +0700

    code fix env

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://react-app-expense-management.vercel.app/',
+  base: '/',
   build: {
     outDir: 'dist', // Đảm bảo đường dẫn mặc định khi build
   },
